a40ce403153a290b20c8a62d61c187e123174e4e
joao castro <joaocastro>
2022-06-24 13:04:25 -0300

    update file

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,8 +1,10 @@
 export function Video (){
-    return (
-    <div className="flex-1">
-        video
-    </div>
+    return ( 
+        <div className="flex-1">
+            <div className="bg-black flex justify-center">
+             <div className="h-full w-full max-w-[1080px] max-h-[60vh]  aspect-video"></div>
+             </div>
+        </div>
     )
  }
  
